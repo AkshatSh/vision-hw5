7361ddd20c3230f86ec254a1219e5072b1353b1f
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-16 20:07:33 -0700

    added models.py

--- models.py
+++ models.py
@@ -37,27 +37,50 @@ class LazyNet(BaseModel):
     def __init__(self):
         super(LazyNet, self).__init__()
         # TODO: Define model here
+        self.layer = nn.Linear(32 * 32 * 3, 10)
 
     def forward(self, x):
         # TODO: Implement forward pass for LazyNet
-        return x
+        return self.layer(x)
         
 
 class BoringNet(BaseModel):
     def __init__(self):
         super(BoringNet, self).__init__()
         # TODO: Define model here
-
+        self.layers = nn.Sequential (
+            nn.Linear(32 * 32 * 3, 120),
+            nn.Linear(120, 84),
+            nn.Linear(84, 10)
+        )
     def forward(self, x):
         # TODO: Implement forward pass for BoringNet
-        return x
+        return self.layers.forward(x)
 
 
 class CoolNet(BaseModel):
     def __init__(self):
         super(CoolNet, self).__init__()
         # TODO: Define model here
+        self.firstLayer = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=8, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.secondLayer = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=8, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=1)
+        )
+        self.lin1 = nn.Linear(2 * 2 * 64, 100)
+        self.lin2 = nn.Linear(100, 10)
 
     def forward(self, x):
         # TODO: Implement forward pass for CoolNet
+        x = self.firstLayer(x)
+        x = self.secondLayer(x)
+        x = x.view(x.size(0), -1)
+        x = self.lin1(x)
+        x = F.sigmoid(x)
+        x = self.lin2(x)
         return x
